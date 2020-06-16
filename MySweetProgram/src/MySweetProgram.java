edbe5a7eabced3e2d201770ef694401aa7f24a54
unknown <parker1097@knights.ucf.edu>
2020-06-16 19:35:25 -0400

    initial commit

---
+++ MySweetProgram/src/MySweetProgram.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class MySweetProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Enter your name");
+		
+		Scanner in = new Scanner(System.in);
+		//String s = in.nextLine();
+		
+		int i = 0; 
+		while(i < 10) {
+			System.out.println("i is:" + i);
+			i++;
+		}
+		
+		in.close();
+		
+	}
+		
+		
+
+}
